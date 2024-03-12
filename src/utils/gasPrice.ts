0f725ec85f04ed718f1d486215995a6bf51df0c9
Ben Smith <bh2smith@gmail.com>
2024-03-12 15:46:32 +0100

    more reliable gas oracle

---
+++ src/utils/gasPrice.ts
@@ -0,0 +1,17 @@
+import { GasPriceResponse } from "../types";
+
+
+export const getFirstNonZeroGasPrice = (response: GasPriceResponse): number | undefined => {
+  // List the properties we're interested in
+  const properties: (keyof GasPriceResponse["data"])[] = ["rapid", "fast", "standard", "slow"];
+
+  // Iterate through the properties and return the first non-zero value
+  for (const property of properties) {
+    if (response.data[property] !== 0) {
+      return response.data[property];
+    }
+  }
+
+  // Return undefined if all values are zero or if none are found
+  return undefined;
+};
