282e3b926a283f7be4499e3d59d6e4f689491568
Ben Smith <bh2smith@gmail.com>
2024-04-25 12:59:28 +0200

    add example script

---
+++ examples/sign-message.ts
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+import { setupNearEthAdapter } from "./setup";
+dotenv.config();
+
+const run = async (): Promise<void> => {
+  const evm = await setupNearEthAdapter();
+  const message = "Hello World";
+  console.log(`Signing "${message}" with ${evm.ethPublicKey()}`);
+
+  const signature = await evm.signMessage(message);
+  console.log("Got Validated Signature", signature);
+};
+
+run();
