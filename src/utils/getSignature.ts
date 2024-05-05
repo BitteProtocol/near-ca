57d6f1f16d3e3c868d74f971f2960762849ce482
Benjamin Smith <bh2smith@users.noreply.github.com>
2024-05-05 18:03:41 -0400

    fix tests

--- src/utils/getSignature.ts
+++ src/utils/getSignature.ts
@@ -1,19 +1,18 @@
-import { JSONRPCResponse, ReceiptOutcome } from "../types/rpc";
+import { JSONRPCResponse } from "../types/rpc";
 
 export async function signatureFromTxHash(
-  txHash: string,
-  accountId: string
+  nodeUrl: string,
+  txHash: string
 ): Promise<[string, string]> {
-  const url: string = "https://archival-rpc.testnet.near.org";
   const payload = {
     jsonrpc: "2.0",
     id: "dontcare",
     method: "EXPERIMENTAL_tx_status",
-    params: [txHash, accountId],
+    params: [txHash, "useless-parameter"],
   };
 
   // Make the POST request with the fetch API
-  const response = await fetch(url, {
+  const response = await fetch(nodeUrl, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -22,20 +21,11 @@ export async function signatureFromTxHash(
   });
 
   const jsonResponse = (await response.json()) as JSONRPCResponse;
+  const base64Sig = jsonResponse.result.status.SuccessValue;
 
-  const receiptsOutcome = jsonResponse.result.receipts_outcome;
-  const successValues = receiptsOutcome.map(
-    (outcome: ReceiptOutcome) => outcome.outcome.status.SuccessValue
-  );
-
-  // Find the first non-undefined value
-  const firstValidValue = successValues.find((value) => value !== undefined);
-
-  if (firstValidValue) {
+  if (base64Sig) {
     // Decode from base64
-    const decodedValue = Buffer.from(firstValidValue, "base64").toString(
-      "utf-8"
-    );
+    const decodedValue = Buffer.from(base64Sig, "base64").toString("utf-8");
     return JSON.parse(decodedValue);
   } else {
     throw new Error("No valid values found in the array.");
