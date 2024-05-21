c674abfe70dcd6a95479689536ac4bf4c9ac9173
Benjamin Smith <bh2smith@users.noreply.github.com>
2024-05-21 11:06:19 +0200

    Update Signature Recovery Return Type (#49)

--- tests/unit/utils.signature.test.ts
+++ tests/unit/utils.signature.test.ts
@@ -8,10 +8,11 @@ describe("utility: get Signature", () => {
 
   it("successful: signatureFromTxHash", async () => {
     const sig = await signatureFromTxHash(url, successHash);
-    expect(sig).toEqual([
-      "03EA06CECA2B7D71F6F4DA729A681B4DE44C6402F5F5BB9FC88C6706959D4FEDD4",
-      "67986E234DEC5D51CF6AED452FE1C4544924218AC20B009F81BAAE53C02AFE76",
-    ]);
+    expect(sig).toEqual({
+      big_r:
+        "03EA06CECA2B7D71F6F4DA729A681B4DE44C6402F5F5BB9FC88C6706959D4FEDD4",
+      big_s: "67986E234DEC5D51CF6AED452FE1C4544924218AC20B009F81BAAE53C02AFE76",
+    });
   });
   it("failed: signatureFromTxHash", async () => {
     await expect(signatureFromTxHash(url, failedHash)).rejects.toThrow(
