3847207078e77b68b10ce83b222d8fac68077191
Ben Smith <bh2smith@gmail.com>
2024-07-28 07:51:52 -0400

    add helper method

--- tests/unit/utils.signature.test.ts
+++ tests/unit/utils.signature.test.ts
@@ -1,4 +1,7 @@
-import { signatureFromTxHash } from "../../src/utils/signature";
+import {
+  signatureFromTxHash,
+  transformSignature,
+} from "../../src/utils/signature";
 
 describe("utility: get Signature", () => {
   const url: string = "https://archival-rpc.testnet.near.org";
@@ -38,5 +41,24 @@ describe("utility: get Signature", () => {
     await expect(signatureFromTxHash(url, failedHash)).rejects.toThrow(
       `No valid values found in transaction receipt ${failedHash}`
     );
+  });
+
+  it("transforms mpcSignature", () => {
+    const signature = {
+      big_r: {
+        affine_point:
+          "0337F110D095850FD1D6451B30AF40C15A82566C7FA28997D3EF83C5588FBAF99C",
+      },
+      s: {
+        scalar:
+          "4C5D1C3A8CAFF5F0C13E34B4258D114BBEAB99D51AF31648482B7597F3AD5B72",
+      },
+      recovery_id: 1,
+    };
+    expect(transformSignature(signature)).toEqual({
+      r: "0x37F110D095850FD1D6451B30AF40C15A82566C7FA28997D3EF83C5588FBAF99C",
+      s: "0x4C5D1C3A8CAFF5F0C13E34B4258D114BBEAB99D51AF31648482B7597F3AD5B72",
+      yParity: 1,
+    });
   });
 });
