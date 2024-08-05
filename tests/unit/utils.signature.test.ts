87e37e89e7e0fbcbac33021865dbe06926932357
Benjamin Smith <bh2smith@users.noreply.github.com>
2024-08-05 10:12:57 +0200

    add test

--- tests/unit/utils.signature.test.ts
+++ tests/unit/utils.signature.test.ts
@@ -1,4 +1,5 @@
 import {
+  signatureFromOutcome,
   signatureFromTxHash,
   transformSignature,
 } from "../../src/utils/signature";
@@ -52,6 +53,21 @@ describe("utility: get Signature", () => {
     expect(transformSignature(signature)).toEqual({
       r: "0x37F110D095850FD1D6451B30AF40C15A82566C7FA28997D3EF83C5588FBAF99C",
       s: "0x4C5D1C3A8CAFF5F0C13E34B4258D114BBEAB99D51AF31648482B7597F3AD5B72",
+      yParity: 1,
+    });
+  });
+
+  it("signatureForOutcome", () => {
+    // Outcome from: wkBWmTXoUgdm7RvdwNTUmtmDZLB14TfXMdyf57UXAaZ
+    const outcome = {
+      status: {
+        SuccessValue:
+          "eyJiaWdfciI6eyJhZmZpbmVfcG9pbnQiOiIwMkVBRDJCMUYwN0NDNDk4REIyNTU2MzE0QTZGNzdERkUzRUUzRDE0NTNCNkQ3OTJBNzcwOTE5MjRFNTFENEMyNDcifSwicyI6eyJzY2FsYXIiOiIxQTlGNjBDMkNDMjM5OEE1MDk3N0Q0Q0E5M0M0MDE2OEU4RjJDRTdBOUM5MEUzNzQ1MjJERjNDNzZDRjU0RjJFIn0sInJlY292ZXJ5X2lkIjoxfQ==",
+      },
+    };
+    expect(signatureFromOutcome(outcome)).toEqual({
+      r: "0xEAD2B1F07CC498DB2556314A6F77DFE3EE3D1453B6D792A77091924E51D4C247",
+      s: "0x1A9F60C2CC2398A50977D4CA93C40168E8F2CE7A9C90E374522DF3C76CF54F2E",
       yParity: 1,
     });
   });
