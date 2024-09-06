36b9acb119d8966b57e4a1b6f22d129baa387032
Benjamin Smith <bh2smith@users.noreply.github.com>
2024-09-06 23:14:21 +0200

    fix error

--- src/network.ts
+++ src/network.ts
@@ -1,5 +1,6 @@
 import { Chain, createPublicClient, http, PublicClient } from "viem";
 import * as chains from "viem/chains";
+import { CHAIN_INFO } from "./constants";
 
 // We support all networks exported by viem
 const SUPPORTED_NETWORKS = createNetworkMap(Object.values(chains));
@@ -92,33 +93,3 @@ function createNetworkMap(supportedNetworks: Chain[]): NetworkMap {
 
   return networkMap;
 }
-
-// A short list of networks with known wrapped tokens.
-const CHAIN_INFO: {
-  [key: number]: { logo?: string; wrappedToken: string };
-} = {
-  11155111: {
-    logo: "https://cryptologos.cc/logos/ethereum-eth-logo.svg?v=014",
-    wrappedToken: "0xD0A1E359811322d97991E03f863a0C30C2cF029C",
-  },
-  1: {
-    logo: "https://cryptologos.cc/logos/ethereum-eth-logo.svg?v=014",
-    wrappedToken: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
-  },
-  100: {
-    logo: "https://cryptologos.cc/logos/gnosis-gno-logo.svg?v=014",
-    wrappedToken: "0x6a023ccd1ff6f2045c3309768ead9e68f978f6e1",
-  },
-  137: {
-    wrappedToken: "0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0",
-  },
-  42161: {
-    wrappedToken: "0x82af49447d8a07e3bd95bd0d56f35241523fbab1",
-  },
-  10: {
-    wrappedToken: "0x4200000000000000000000000000000000000006",
-  },
-  8453: {
-    wrappedToken: "0x4200000000000000000000000000000000000006",
-  },
-};
